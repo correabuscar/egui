b43ed99dc1a72f00d58736251c6355e441ce1e82
correabuscar <correabuscar+githubcommits@gmail.com>
2024-03-05 00:30:27 +0100

    weird keyboard behaviour
    or I'm missing something!

--- examples/keyboard_events/src/main.rs
+++ examples/keyboard_events/src/main.rs
@@ -32,15 +32,36 @@ impl eframe::App for Content {
                     ui.label(&self.text);
                 });
 
+            let pressed:bool;
+            let human_pressed:bool;
+            let held:bool;
+            let released:bool;
+
+
             if ctx.input(|i| i.key_pressed(Key::A)) {
-                self.text.push_str("\nPressed");
+                pressed=true;
+            } else {
+                pressed=false;
             }
             if ctx.input(|i| i.key_down(Key::A)) {
-                self.text.push_str("\nHeld");
-                ui.ctx().request_repaint(); // make sure we note the holding.
+                held=true;
+            } else {
+                held=false;
+            }
+            if pressed && !held  {
+                human_pressed=true;
+            } else {
+                human_pressed=false;
             }
             if ctx.input(|i| i.key_released(Key::A)) {
-                self.text.push_str("\nReleased");
+                released=true;
+            } else {
+                released=false;
+            }
+            //self.text=String::from(format!("{},{},{},{}",human_pressed as i8,pressed as i8,held as i8,released as i8));
+            self.text.push_str(&format!("HP:{},P:{},H:{},R:{}\n",human_pressed as i8,pressed as i8,held as i8,released as i8)[..]);
+            if held {
+                ui.ctx().request_repaint(); // make sure we note the holding.
             }
         });
     }
